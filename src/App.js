97b7945b1239ec8f0fc49d329cfe31f994ee2daf
mdos-san <mdossan.perso@gmail.com>
2017-04-02 17:50:35 +0200

    Render little posters in list

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 
 import Latest from './Latest'
 import Search from './Search'
+import Top from './Top'
 
 class App extends Component {
 	constructor() {
 		super();
-		this.state = { view: "Latest" };
+		this.state = { view: "Top" };
 		this.header = 
 			<div>
-				<a href="#" onClick={() => (this.setView('Latest'))}>Latest</a>
+				<a href="#" onClick={() => (this.setView('Top'))}>Top rated!</a>
 				&nbsp;
 				<a href="#" onClick={() => (this.setView('Search'))}>Search</a>
+				&nbsp;
+				<a href="#" onClick={() => (this.setView('Latest'))}>Latest</a>
 			</div>;
 	}
 	render() {
@@ -19,13 +22,23 @@ class App extends Component {
 			return (
 				<div>
 					{this.header}
+					<br/>
 					<Latest/>
+				</div>
+			);
+		else if (this.state.view === "Top")
+			return (
+				<div>
+					{this.header}
+					<br/>
+					<Top/>
 				</div>
 			);
 		else if (this.state.view === "Search")
 			return (
 				<div>
 					{this.header}
+					<br/>
 					<Search/>
 				</div>
 			);
--- src/Top.js
+++ src/Top.js
@@ -3,26 +3,19 @@ import React, { Component } from 'react';
 import Detail 				from './Detail.js';
 import $					from '../public/jquery.js'
 
-class Search extends Component {
+class Top extends Component {
 	constructor() {
 		super();
 		this.apiKey = "07b26d975afb0480ef4b4c33e129c700";
-		this.searchApi = this.searchApi.bind(this);
 		this.state = { r: undefined };
-	}
-
-	searchApi(e) {
-		if (e.target.value !== "") {
-			console.log(e.target.value);
-			var that = this;
-			$.ajax({
-				method:	"GET",
-				url:	"https://api.themoviedb.org/3/search/movie?api_key=" + that.apiKey + "&query=" + e.target.value
-			}).then(function (response) {
-				console.log(response);
-				that.setState({ r: response });
-			});
-		}
+		var that = this;
+		$.ajax({
+			method:	"GET",
+			url:	"https://api.themoviedb.org/3/movie/top_rated?api_key=" + that.apiKey
+		}).then(function (response) {
+			console.log(response);
+			that.setState({ r: response });
+		});
 	}
 
 	getDetails(item) {
@@ -54,8 +47,7 @@ class Search extends Component {
 		else
 			return (
 				<div>
-					<h2>Search</h2>
-					<input onChange={this.searchApi} type="text" placeholder="Search a movie..."/>
+					<h2>Top rated movies</h2>
 					<table>
 						<tbody>
 						{this.state.r.results.map((item) => (
@@ -76,4 +68,4 @@ class Search extends Component {
 	}
 }
 
-export default Search;
+export default Top;
